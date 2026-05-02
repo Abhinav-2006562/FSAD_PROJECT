108e49cb17e576afe3fc3ffb30dcac66e87efa7d
Abhinav-2006562 <2400030317@kluniversity.in>
2026-05-02 15:37:36 +0530

    Use Railway backend API

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://fsadbackend-production-10f0.up.railway.app/api",
 });
 
 // Attach JWT token automatically
